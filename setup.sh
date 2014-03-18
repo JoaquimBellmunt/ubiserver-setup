76c9345b6b6427d5333f0d80b711b4522768765a
Thibaut Tiberghien <thibaut.tiberghien@ipal.cnrs.fr>
2014-03-18 15:45:05 +0800

    Add creation of user with sudo rights

--- setup.sh
+++ setup.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 # setup file for configuring ubismart server
 
+# create user and add to sudoers
+sudo adduser ubi
+sudo apt-get -y install sudo
+sudo usermod -a -G sudo ubi
+
+# change to this user
+su - ubi
+cd ~/
+
+
