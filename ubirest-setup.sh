34e296c969b9edeeec8a9279961506fc8bf6fc5d
Thibaut Tiberghien <thibaut.tiberghien@ipal.cnrs.fr>
2014-03-19 16:56:03 +0800

    Add wsgi for apache and virtualenv for python

--- ubirest-setup.sh
+++ ubirest-setup.sh
@@ -12,8 +12,25 @@ sudo apt-get -y install fabric
 wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | sudo python
 rm setuptools*.zip
 
+# install mod_wsgi for Apache
+sudo apt-get -y install libapache2-mod-wsgi
+#sudo sh -c 'echo "LoadModule wsgi_module/mod_wsgi.so" >> /etc/apache2/httpd.conf'   # not for Ubuntu. Make sure wsgi is present in /etc/apache2/mods_enabled
+sudo sh -c 'cat add2httpd.conf >> /etc/apache2/httpd.conf'
+sudo service apache2 restart
+
+# create ubirest folder in www
+sudo mkdir /var/www/ubirest/
+sudo chown ubi:ubi /var/www/ubirest/
+
+# install virtualenv for python and set it in ubirest folder
+sudo apt-get -y install python-virtualenv
+cd /var/www/ubirest/
+virtualenv --distribute env
+cd ~/
+
 # update fabfile
 # TODO
 
 # deploy with fabric
 fab clean pack deploy
+
